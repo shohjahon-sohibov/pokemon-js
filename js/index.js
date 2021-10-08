3881bbc9aeeaa55b328712ebf0bd5659f9a82b3c
shohjahon-sohibov <sohibov.s@bk.ru>
2021-10-08 11:27:32 +0500

    pokemon-js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var elPokemons = $_('.pokemons');
+var elPokemonTemplate = $_('#pokemon-card-template').content;
+
+elPokemons.innerHTML = '';
+
+var createElementPokemon = function(pokemon) {
+  var elNewPokemon = elPokemonTemplate.cloneNode(true);
+
+  elNewPokemon.querySelector('.pokemon__img').src = pokemon.img;
+  elNewPokemon.querySelector('.pokemon__img').alt = pokemon.name;
+  elNewPokemon.querySelector('.pokemon__name').textContent = pokemon.name;
+  elNewPokemon.querySelector('.pokemon__type').textContent = pokemon.type.join(', ');
+
+  return elNewPokemon
+};
+
+pokemons.forEach(function(pokemon) {
+  elPokemons.appendChild(createElementPokemon(pokemon));
+});
